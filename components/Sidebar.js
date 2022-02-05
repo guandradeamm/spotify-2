f3d87390f93fc968a8bc7f858d650d082749b1dd
Gustavo Andrade <gustavo_alex3000@hotmail.com>
2022-02-05 19:13:48 -0300

    feat: add sign in page

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -7,11 +7,18 @@ import {
   HeartIcon,
   RssIcon,
 } from '@heroicons/react/outline'
+import { signOut } from 'next-auth/react'
 
 function Sidebar() {
   return (
     <div className="border-r border-gray-900 p-5 text-sm text-gray-400">
       <div className="space-y-4">
+        <button
+          className="flex items-center space-x-2 hover:text-white"
+          onClick={() => signOut()}
+        >
+          <p>Log out</p>
+        </button>
         <button className="flex items-center space-x-2 hover:text-white">
           <HomeIcon className="h-5 w-5" />
           <p>Home</p>
--- pages/login.js
+++ pages/login.js
@@ -1,15 +1,26 @@
 import React from 'react'
 import { getProviders, signIn } from 'next-auth/react'
 
-function login() {
+function Login({ providers }) {
   return (
-    <div>
-      <h1>This is login page</h1>
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-black">
+      <img className="mb-5 w-52" src="https://links.papareact.com/9xl" alt="" />
+
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button
+            className="rounded-full bg-[#18D860] p-5 text-white"
+            onClick={() => signIn(provider.id, { callbackUrl: '/' })}
+          >
+            Login with {provider.name}
+          </button>
+        </div>
+      ))}
     </div>
   )
 }
 
-export default login
+export default Login
 
 export async function getServerSideProps() {
   const providers = await getProviders()
